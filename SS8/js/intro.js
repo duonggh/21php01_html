0d2b64ff9a15cc2b78682e8431a5e49dcaafd726
Duong <duong28194@gmail.com>
2021-04-05 18:08:15 +0700

    bvn ss8

---
+++ SS8/js/intro.js
@@ -0,0 +1,7 @@
+function testExternal() {
+	document.getElementById("test_external").innerHTML = "Hello demo external";
+}
+
+function changeImage(){
+	document.getElementById("change_image").src="img/rnd2.jpg";
+}
